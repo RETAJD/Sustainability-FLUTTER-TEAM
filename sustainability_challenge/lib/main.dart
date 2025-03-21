ba0c693d102ce853cbc5d954b0dc552d54742d1f
MARCINRETAJCZYK <marcinretajczyk@MacBook-Air-MARCINRETAJCZYK.local>
2025-03-21 10:15:37 +0100

    add main interface and section screens to visualisations

--- sustainability_challenge/lib/main.dart
+++ sustainability_challenge/lib/main.dart
@@ -1,53 +1,114 @@
 import 'package:flutter/material.dart';
-import 'data/occupancy_data_loader.dart';
-import 'widgets/occupancy_chart.dart';
+import 'package:sustainability_challenge/screens/screen_occupancy.dart';
+
+//import 'widgets/plugs_chart.dart'; // Przykładowy plik do obsługi wykresów dla "Plugs"
+//import 'widgets/smart_meter_chart.dart'; // Przykładowy plik do obsługi wykresów dla "Smart Meter"
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  List<double>? summerData;
-  List<double>? winterData;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Loading data
-    Future.wait([
-      load15MinAveragedData(
-        "assets/data_eth/occupancy/01_occupancy_csv/01_summer.csv",
-      ),
-      load15MinAveragedData(
-        "assets/data_eth/occupancy/01_occupancy_csv/01_winter.csv",
-      ),
-    ]).then((data) {
-      setState(() {
-        summerData = data[0]; // Dane letnie
-        winterData = data[1]; // Dane zimowe
-      });
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Occupancy Visualization")),
-        body: Center(
-          child:
-              (summerData == null || winterData == null)
-                  ? CircularProgressIndicator() // Wczytywanie danych
-                  : OccupancyChart(
-                    summerData: summerData!,
-                    winterData: winterData!,
-                  ), // Wykres z dwoma zestawami danych
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Eco-Friendly App"),
+        backgroundColor: Colors.green.shade700, // Kolor ekologiczny
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Select option:',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.green.shade800, // Zielony kolor
+              ),
+            ),
+            SizedBox(height: 50),
+            // Przycisk Occupancy
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => OccupancyScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green.shade500, // Kolor przycisku
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30), // Zaokrąglone rogi
+                ),
+                elevation: 5,
+              ),
+              child: Text(
+                'Occupancy',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+            SizedBox(height: 20),
+            // Przycisk Plugs
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => OccupancyScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue.shade400, // Kolor przycisku
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                elevation: 5,
+              ),
+              child: Text(
+                'Plugs',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+            SizedBox(height: 20),
+            // Przycisk Smart Meter
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => OccupancyScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.brown.shade300, // Kolor przycisku
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                elevation: 5,
+              ),
+              child: Text(
+                'Smart Meter',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+          ],
         ),
       ),
     );
