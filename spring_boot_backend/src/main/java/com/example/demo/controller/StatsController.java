7bab34cd639ddbfabff1ab9b21c94e8f5a73358a
unknown <salonisalian93@gmail.com>
2025-04-28 14:56:19 +0400

    Aggregator

--- spring_boot_backend/src/main/java/com/example/demo/controller/StatsController.java
+++ spring_boot_backend/src/main/java/com/example/demo/controller/StatsController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.DataAggregatorService;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.DataAggregatorService;
 
 @RestController
 @RequestMapping("/api")
